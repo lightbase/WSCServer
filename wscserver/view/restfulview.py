c452bde33acf674f10a808dac9d913b434aea802
antonygc91 <antonygc91@gmail.com>
2014-09-29 17:44:25 -0300

    build computers json and writing to file

--- wscserver/view/restfulview.py
+++ wscserver/view/restfulview.py
@@ -1,103 +1,149 @@
-from pyramid_restler.view import RESTfulView
-from collections import defaultdict
 import json
 import hashlib
-from itertools import groupby
 
-class CustomRESTfulView(RESTfulView):
+from pyramid.response import Response
+from wscserver.model import session
 
-    """
-    Classe com parametros personalizados de view dos registros
-    para as classes WMI
-    """
 
-    # Computer class and properties filters
-    computer_filter = {
-        "OperatingSystem": [
-            "Caption",
-            "Version",
-            "InstallDate",
-            # quantidade ???
-        ],
-        "Win32_Processor": [
-            "Manufacturer",
-            "Caption",
-            "NumberOfLogicalProcessors",
-            # idade ??
-        ],
-        "Win32_BIOS": [
-            "Manufacturer",
-        ],
+FILE_BEGIN = '{"results":['
+FILE_END = '], "result_count": %s}'
+
+# Computer class and properties filters
+COMPUTER_FILTER = {
+    "OperatingSystem": [
+        "Caption",
+        "Version",
+        "InstallDate",
+        # quantidade ???
+    ],
+    "Win32_Processor": [
+        "Manufacturer",
+        "Caption",
+        "NumberOfLogicalProcessors",
+        # idade ??
+    ],
+    "Win32_BIOS": [
+        "Manufacturer",
+    ],
+    "SoftwareList": []
+}
+
+FILTER_KEYS = str(tuple(COMPUTER_FILTER.keys()))
+
+
+def viewcoleta(request):
+
+    #create index idx_id_computador on computador_coleta(id_computador);
+
+    limit = request.params.get('limit', 'NULL')
+
+    stmt1 = """
+        SELECT 
+            id_computador 
+        FROM computador_coleta 
+        GROUP BY id_computador
+        LIMIT {};
+    """.format(limit)
+
+    computer_ids = session.execute(stmt1)
+
+
+    stmt2 = """
+        SELECT classe.nm_class_name,
+               cp.nm_property_name,
+               cc.te_class_property_value
+        FROM computador_coleta AS cc
+            INNER JOIN class_property as cp ON (cc.id_class_property =
+                cp.id_class_property)
+            INNER JOIN classe ON (classe.id_class = cp.id_class)
+            WHERE cc.id_computador = {} AND
+            classe.nm_class_name IN {};
+        """
+
+    with open('/tmp/coleta.json', 'w') as f:
+
+        f.write(FILE_BEGIN)
+
+        count = computer_ids.rowcount
+
+        for (id_computador, ) in computer_ids.fetchall():
+
+            computer_attributes = session.execute(
+                stmt2.format(id_computador, FILTER_KEYS)).fetchall()
+            computer_json = build_computer_json(computer_attributes)
+
+            f.write(computer_json)
+            count -= 1
+
+            if count is not 0:
+                f.write(',')
+
+        f.write(FILE_END % computer_ids.rowcount)
+
+    return Response('ok')
+   #return Response(
+   #    content_type='application/json',
+   #    content_disposition='filename=coleta.json',
+   #    app_iter=[open('/var/antony/coleta.json', 'rb').read()]
+   #)
+
+
+
+def build_computer_json(computer_group):
+
+    computer = {
+        "OperatingSystem": {},
+        "Win32_Processor": {},
+        "Win32_BIOS": {},
+        "SoftwareList": []
     }
 
-    def render_json(self, collection):
+    for class_, property_, property_value in computer_group:
 
-        """
-        :param collection: List of <wscserver.model.Coleta.Coleta object>
-        collection element attributes example:
+        if class_ == 'SoftwareList':
+            if property_value.lower().find('office') > -1:
+                computer[class_].append(property_value)
+            elif property_value.lower().find('microsoft') > -1:
+                computer[class_].append(property_value)
+            continue
 
-        "id_class": 3,
-        "id_computador_coleta_historico": 1,
-        "nm_class_name": "NetworkAdapterConfiguration",
-        "nm_property_name": "Description",
-        "id_computador_coleta": 1,
-        "id_class_property": 6,
-        "dt_hr_inclusao": "2014-07-18 12:34:43",
-        "id_computador": 3,
-        "te_class_property_value": " "Intel(R) 82579V Gigabit Network Connection"
-        """
+        elif property_ not in COMPUTER_FILTER[class_]:
+            continue
 
-        def keyfunc(obj):
-            # Will group by computer ID
-            return int(obj.id_computador)
+        else:
+            prefixed_property = class_ + '_' + property_
+            computer[class_][prefixed_property] = property_value
 
-        # Sort list before grouping
-        collection = sorted(collection, key=keyfunc)
+    return json.dumps(computer)
 
-        # Create result list
-        computers = [ ]
+def zipcoleta():
+    """Recebe o json da coleta em um arquivo e zipa"""
+    coleta = requests.get('http://localhost/wscserver/rest/coleta')
 
-        # Navigate groups
-        for id_computador, computer_group in groupby(collection, keyfunc):
+    tmpdir = tempfile.gettempdir()
+    os.chdir(tmpdir)
+    f = open('coleta.json','r')
+    arquivozip = 'coleta.zip'
+    with zipfile.ZipFile(arquivozip, 'w') as myzip:
+        myzip.write(f.name)
+    f.close()
+    os.remove('coleta.json')
+    filepath = os.path.abspath(arquivozip)
+    return filepath
+'''
+def zipcoleta():
+    """Recebe o json da coleta em um arquivo e zipa"""
+    coleta = requests.get('http://localhost/wscserver/rest/coleta')
 
-            computer = { }
-
-            for collection_element in list(computer_group):
-
-                class_ = collection_element.nm_class_name
-                property_ = collection_element.nm_property_name
-                property_value = collection_element.te_class_property_value
-
-                if class_ == 'SoftwareList':
-                    if computer.get(class_) is None:
-                        computer[class_] = [ ]
-                    elif property_value.lower().find('office') > -1:
-                        computer[class_].append(property_value)
-                    elif property_value.lower().find('microsoft') > -1:
-                        computer[class_].append(property_value)
-                    continue
-
-                elif class_ not in self.computer_filter \
-                    or property_ not in self.computer_filter[class_]:
-                    # Filter classes and properties 
-                    continue
-
-                elif computer.get(class_) is None:
-                    computer[class_] = { }
-                else:
-                    prefixed_property = class_ + '_' + property_
-                    computer[class_][prefixed_property] = property_value
-
-            computers.append(computer)
-
-        response = {
-            'results': computers,
-            'result_count': len(computers)
-        }
-
-        return dict(
-            body=json.dumps(response),
-            content_type='application/json'
-        )
-
-
+    path = tempfile.gettempdir()
+    os.chdir(path)
+    f = open('coleta.json','w')
+    f.write(coleta.text)
+    arquivozip = 'coleta.zip'
+    with zipfile.ZipFile(arquivozip, 'w') as myzip:
+        myzip.write(f.name)
+    f.close()
+    os.remove('coleta.json')
+    filepath = os.path.abspath(arquivozip)
+    return filepath
+'''
